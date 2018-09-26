092545493c42d8b8c4bcd3e9f73ff4dbbf602685
Ilya Lovriakov <ilya.lovriakov@booking.com>
2018-09-26 20:27:49 +0200

    updating source modal

--- src/scripts/globals.js
+++ src/scripts/globals.js
@@ -2,7 +2,7 @@ const Globals = {
   imagesElements: document.querySelectorAll('.memorial--image img'),
   videoElement: document.querySelector('video.user-video'),
   closeSocialModal() {
-    document.querySelector('#modal-source button.modal__close').click();
+    document.querySelector('#modal-source .modal__overlay').click();
   },
   simpleMemorialElement: document.querySelector('.memorial.simple'),
   simpleMemorialText: document.querySelector('.memorial.simple .memorial--text'),
